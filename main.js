b87b377eb4b38dfa4af4840f1655383aae52ad6b
fontanavitor1 <145573605+fontanavitor1@users.noreply.github.com>
2025-06-17 07:55:14 -0300

    17/06

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ textos[i].classList.add("ativo");
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("05-10-2025T00:00:00");
+const tempoObjetivo1 = new Date("2025-12-20T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
